177c2545d1fdb6bb0118a2451249ec87878751f9
Chris <javvorski.krzysztof@gmail.com>
2026-02-23 15:57:28 +0100

    fix: remove duplicate email validator in users table

--- src/Model/Table/UsersTable.php
+++ src/Model/Table/UsersTable.php
@@ -217,24 +217,12 @@ class UsersTable extends BaseUsersTable
 
     public function validationDefault(Validator $validator): Validator
     {
-        $validator = parent::validationDefault($validator);
-
-        $validator
-            ->requirePresence('email', 'create')
-            ->notEmptyString('email', 'E-mail jest wymagany.')
-            ->email('email', false, 'Podaj poprawny adres e-mail.');
-
-        return $validator;
+        return parent::validationDefault($validator);
     }
 
     public function validationRegister(Validator $validator)
     {
         $validator = parent::validationRegister($validator);
-
-        $validator
-            ->requirePresence('email', 'create')
-            ->notEmptyString('email', 'E-mail jest wymagany.')
-            ->email('email', false, 'Podaj poprawny adres e-mail.');
 
         $validator
             ->add('additional_data', 'registrationCompanyPrefillRequired', [
